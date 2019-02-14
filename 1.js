5474845f7bd37c899c0dafd917c2f448310ab79d
unknown <longphinguyen07@gmail.com>
2019-02-14 10:49:03 +0700

    first-commit

---
+++ 1.js
@@ -0,0 +1,11 @@
+﻿ $(function(){
+ 	$('.cacngonngu').slideUp();
+ 	$('.top-ngonngu b').click(function(event) {
+ 		/* Act on the event */
+ 		$(this).next().slideToggle(300);
+ 	});
+
+ 	//kích hoạt wow
+ 	new WOW().init();
+})  
+ 
